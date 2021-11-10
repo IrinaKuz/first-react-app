666e106ed29b9ecf3934c765fb1ca19f8de1c66f
IrinaKuz <ira.kuznets@gmail.com>
2021-11-09 21:43:17 -0500

    Implemented MyCard component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 import "bootstrap/dist/css/bootstrap.min.css";
+import MyCard from './CardComponent';
 
 class Menu extends Component {
-
     constructor(props) {
         super(props);
+        this.onDishSelect = this.onDishSelect.bind(this);
 
         this.state = {
             selectedDish: null,
@@ -16,9 +17,9 @@ class Menu extends Component {
         }
     }
 
-    onDishSelect(i) {
+    onDishSelect(dish) {
         this.setState({
-            selectedDish: i,
+            selectedDish: dish,
             modal: true
         })
     }
@@ -30,17 +31,13 @@ class Menu extends Component {
         })
     }
 
+    
     render() {
         const menu = this.props.dishes
                         .map(i => {
                             return (
                                 <div key={i.id} className="col-12 col-sm-6 col-md-3 p-1">
-                                    <Card onClick={() => this.onDishSelect(i)}>
-                                        <CardImg width="100%" src={i.image} alt={i.name}/>
-                                        <CardImgOverlay>
-                                            <CardTitle tag="h3">{i.name}</CardTitle>
-                                        </CardImgOverlay>
-                                    </Card>
+                                    <MyCard dish={i} onDishSelect={this.onDishSelect}/>
                                 </div>
                             );
                         });
@@ -53,7 +50,8 @@ class Menu extends Component {
                 <div className="row">
                 <Modal isOpen={this.state.modal} toggle={() => this.toggle()}>
                     <ModalHeader toggle={() => this.toggle()}>
-                        {this.state.selectedDish === null ? '' : this.state.selectedDish.name}
+                        {this.state.selectedDish === null ? '' : <img className="m-2" src={this.state.selectedDish.image} />}   
+                        {this.state.selectedDish === null ? '' : this.state.selectedDish.name} 
                     </ModalHeader>
                     <ModalBody>
                         {this.state.selectedDish === null ? '': this.state.selectedDish.description}
