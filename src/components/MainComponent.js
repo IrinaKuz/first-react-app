6733e1dd4edc8177a3095733aea624e1258e72d2
IrinaKuz <ira.kuznets@gmail.com>
2021-11-17 10:50:30 -0500

    Intro to redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,52 +5,30 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments';
-import { LEADERS } from '../shared/leaders';
-import { PROMOTIONS } from '../shared/promotions';
 import Home from './HomeComponent';
 import DishDetail from './DishDetail';
 import {
     BrowserRouter as Router,
     Switch, 
-    Route
+    Route,
+    withRouter
   } from "react-router-dom";
 import { Container } from 'reactstrap';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+    return {
+        dishes: state.dishes,
+        comments: state.comments,
+        promotions: state.promotions,
+        leaders: state.leaders
+    }   
+}
 
 class Main extends Component{
   constructor(props) {
     super(props);
-    this.onDishSelect = this.onDishSelect.bind(this);
-    this.toggle = this.toggle.bind(this);
-
-    this.state = {
-      dishes: DISHES,
-      selectedDish: null,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS,
-      // states for modal
-      initialModalState: false,
-      modal: false,
-      fade: true,
-    }
   }
-
-    onDishSelect(dish) {
-        this.setState({
-            selectedDish: dish,
-            modal: true
-        })
-    }
-
-    // toggling state of the model
-    toggle() {
-        this.setState({
-            modal: !this.state.modal,
-            fade: !this.state.fade
-        })
-    }
 
     render() {
         console.log('HomePage!')
@@ -58,9 +36,9 @@ class Main extends Component{
             return(
                 <Container>
                     <Home 
-                        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-                        promotion={this.state.promotions.filter((promotion) => promotion.featured)[0]}
-                        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                        dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+                        promotion={this.props.promotions.filter((promotion) => promotion.featured)[0]}
+                        leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                     />
                 </Container>
             )
@@ -68,31 +46,28 @@ class Main extends Component{
 
         const DishWithId = ({match}) => {
             return (
-                <DishDetail item={this.state.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}/>
+                <DishDetail item={this.props.dishes.filter(dish => dish.id === parseInt(match.params.dishId, 10))[0]}/>
             )
         }
 
         return (
-                    <div>
-                    <Header />
-                    <Switch>
-                        <Route exact path="/menu" component={
-                            () => <Menu dishes={this.state.dishes} 
-                                        onDishSelect={this.onDishSelect}
-                                        isOpen={this.state.modal}
-                                        toggle={() => this.toggle()}
-                            /> 
-                        } />
-                        <Route path="/menu/:dishId" component={DishWithId} />
-                        <Route exact path="/about" component={() => <About leaders={this.state.leaders} /> }/>
-                        <Route exact path="/contactus" component={Contact} />
-                        <Route component={HomePage} />
-                    </Switch>
-                    <Footer />
-                    </div> 
+            <div>
+                <Header />
+                <Switch>
+                    <Route exact path="/menu" component={
+                        () => <Menu dishes={this.props.dishes}
+                    /> 
+                            } />
+                    <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route exact path="/about" component={() => <About leaders={this.props.leaders} /> }/>
+                    <Route exact path="/contactus" component={Contact} />
+                    <Route component={HomePage} />
+                </Switch>
+                <Footer />
+            </div> 
         );
     }
 }
 
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
